5f18bb271713b78aff6296e72c4af967f412ced9
Daniel Hazelbaker <daniel@blueboxmoon.com>
2017-08-07 15:29:31 -0700

    Added Fundraising Donations List block.

---
+++ Controls/FundraisingDonationsList.ascx
@@ -0,0 +1,32 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="FundraisingDonationsList.ascx.cs" Inherits="Plugins.com_shepherdchurch.Misc.FundraisingDonationsList" %>
+
+<asp:UpdatePanel ID="upnlContent" runat="server">
+    <ContentTemplate>
+        <asp:Panel ID="pnlDetails" runat="server" CssClass="panel panel-block">
+            <div class="panel-heading">
+                <h3 class="panel-title"><i class="fa fa-money"></i> Fundraising Donations</h3>
+            </div>
+
+            <div class="panel-body">
+                <div class="grid grid-panel">
+                    <Rock:Grid ID="gDonations" runat="server" DisplayType="Full" AllowSorting="true" EmptyDataText="No Donations Found" PersonIdField="DonorId" RowItemText="Donations" DataKeyNames="DonorId">
+                        <Columns>
+                            <Rock:SelectField />
+                            <Rock:PersonField DataField="Donor" HeaderText="Donor" SortExpression="Donor.LastName, Donor.NickName" />
+                            <Rock:RockBoundField DataField="Address" HeaderText="Donor Address" HtmlEncode="false" />
+                            <Rock:RockBoundField DataField="Donor.Email" HeaderText="Donor Email" SortExpression="Donor.Email" />
+                            <Rock:RockTemplateField  HeaderText="Participant" SortExpression="Participant.Person.LastName, Participant.Person.NickName">
+                                <ItemTemplate>
+                                    <a href="/GroupMember/<%# Eval( "Participant.Id" ) %>"><%# Eval( "Participant.Person.FullName" ) %></a>
+                                    <a href="/Person/<%# Eval( "Participant.Person.Id" ) %>" class="margin-l-sm btn btn-sm btn-default"><i class="fa fa-user"></i></a>
+                                </ItemTemplate>
+                            </Rock:RockTemplateField>
+                            <Rock:DateField DataField="Date" HeaderText="Date" HeaderStyle-HorizontalAlign="Right" SortExpression="Date" />
+                            <Rock:CurrencyField DataField="Amount" HeaderText="Amount" HeaderStyle-HorizontalAlign="Right" SortExpression="Amount" />
+                        </Columns>
+                    </Rock:Grid>
+                </div>
+            </div>
+        </asp:Panel>
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ Controls/FundraisingDonationsList.ascx.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Dynamic;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using Rock;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web;
+using Rock.Web.Cache;
+using Rock.Web.UI;
+using Rock.Web.UI.Controls;
+
+namespace Plugins.com_shepherdchurch.Misc
+{
+    [DisplayName( "Fundraising Donations List" )]
+    [Category( "Shepherd Church > Misc" )]
+    [Description( "Lists donations in a grid for the current fundraising opportunity or participant." )]
+
+    [ContextAware]
+    public partial class FundraisingDonationsList : RockBlock
+    {
+        #region Base Method Overrides
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+
+            base.BlockUpdated += FundraisingDonationsList_BlockUpdated;
+
+            gDonations.GridRebind += gDonations_GridRebind;
+        }
+
+        /// <summary>
+        /// Initialize basic information about the page structure and setup the default content.
+        /// </summary>
+        /// <param name="sender">Object that is generating this event.</param>
+        /// <param name="e">Arguments that describe this event.</param>
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+
+            if ( !Page.IsPostBack )
+            {
+                ShowDetails();
+            }
+        }
+
+        #endregion
+
+        #region Core Methods
+
+        /// <summary>
+        /// Show the block content.
+        /// </summary>
+        protected void ShowDetails()
+        {
+            var group = ContextEntity<Group>();
+            var groupMember = ContextEntity<GroupMember>();
+
+            if ( groupMember != null )
+            {
+                group = groupMember.Group;
+            }
+
+            pnlDetails.Visible = false;
+
+            //
+            // Only show the panel and content if the group type is a fundraising opportunity.
+            //
+            if ( group != null && group.GroupTypeId == GroupTypeCache.Read( Rock.SystemGuid.GroupType.GROUPTYPE_FUNDRAISINGOPPORTUNITY.AsGuid() ).Id )
+            {
+                pnlDetails.Visible = true;
+                BindGrid();
+            }
+        }
+
+        /// <summary>
+        /// Bind the grid to the donations that should be visible for the proper context.
+        /// </summary>
+        protected void BindGrid()
+        {
+            var rockContext = new RockContext();
+            var groupMemberService = new GroupMemberService( rockContext );
+            var financialTransactionDetailService = new FinancialTransactionDetailService( rockContext );
+            var entityTypeIdGroupMember = EntityTypeCache.GetId<GroupMember>();
+            Dictionary<int, GroupMember> groupMembers;
+
+            //
+            // Get the donations for the entire opportunity group or for just the
+            // one individual being viewed.
+            //
+            if ( ContextEntity<Group>() != null )
+            {
+                var group = ContextEntity<Group>();
+
+                groupMembers = groupMemberService.Queryable()
+                    .Where( m => m.GroupId == group.Id )
+                    .ToDictionary( m => m.Id );
+
+                gDonations.Columns.OfType<RockTemplateField>().Where( c => c.HeaderText == "Participant" ).ToList().ForEach( c => c.Visible = true );
+                gDonations.Columns.OfType<DateField>().Where( c => c.HeaderText == "Date" ).ToList().ForEach( c => c.Visible = false );
+            }
+            else
+            {
+                var groupMember = ContextEntity<GroupMember>();
+
+                groupMembers = new Dictionary<int, GroupMember> { { groupMember.Id, groupMember } };
+
+                gDonations.Columns.OfType<RockTemplateField>().Where( c => c.HeaderText == "Participant" ).ToList().ForEach( c => c.Visible = false );
+                gDonations.Columns.OfType<DateField>().Where( c => c.HeaderText == "Date" ).ToList().ForEach( c => c.Visible = true );
+            }
+
+            //
+            // Get the list of donation entries for the grid that match the list of members.
+            //
+            var groupMemberIds = groupMembers.Keys.ToList();
+            var donations = financialTransactionDetailService.Queryable()
+                .Where( d => d.EntityTypeId == entityTypeIdGroupMember && groupMemberIds.Contains( d.EntityId.Value ) )
+                .ToList()
+                .Select( d => new
+                {
+                    DonorId = d.Transaction.AuthorizedPersonAlias.PersonId,
+                    Donor = d.Transaction.AuthorizedPersonAlias.Person,
+                    Participant = groupMembers[d.EntityId.Value],
+                    Amount = d.Amount,
+                    Address = d.Transaction.AuthorizedPersonAlias.Person.GetHomeLocation( rockContext ).ToStringSafe().ConvertCrLfToHtmlBr(),
+                    Date = d.Transaction.TransactionDateTime
+                } ).AsQueryable();
+
+            //
+            // Apply user sorting or default to donor name.
+            //
+            if ( gDonations.SortProperty != null )
+            {
+                donations = donations.Sort( gDonations.SortProperty );
+            }
+            else
+            {
+                donations = donations.Sort( new SortProperty { Property = "Donor.LastName, Donor.NickName" } );
+            }
+
+            gDonations.ObjectList = donations.Select( d => d.Donor )
+                .Cast<object>()
+                .ToDictionary( p => ( ( Person ) p ).Id.ToString() );
+
+            gDonations.DataSource = donations.ToList();
+            gDonations.DataBind();
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        /// <summary>
+        /// Handles the BlockUpdated event of the control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void FundraisingDonationsList_BlockUpdated( object sender, EventArgs e )
+        {
+            ShowDetails();
+        }
+
+        /// <summary>
+        /// Handles the GridRebind event of the control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="GridRebindEventArgs"/> instance containing the event data.</param>
+        private void gDonations_GridRebind( object sender, Rock.Web.UI.Controls.GridRebindEventArgs e )
+        {
+            BindGrid();
+        }
+
+        #endregion
+    }
+}
