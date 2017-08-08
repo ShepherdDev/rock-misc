45902c5b3807cede9d764fa3c85619592823e88a
Daniel Hazelbaker <daniel@blueboxmoon.com>
2017-08-08 14:25:10 -0700

    Added LavaSqlDashboardWidget and REST API to support it.

---
+++ Controls/LavaSqlDashboardWidget.ascx.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel;
+using Rock;
+using Rock.Attribute;
+using Rock.Model;
+using Rock.Reporting.Dashboard;
+using Rock.Web.Cache;
+using Rock.Web.UI.Controls;
+
+namespace Plugins.com_shepherdchurch.Misc
+{
+    /// <summary>
+    /// NOTE: Most of the logic for processing the Attributes is in com.ShepherdChurch.Misc.Rest.Blocks.GetHtmlForSqlBlock
+    /// </summary>
+    /// <seealso cref="Rock.Reporting.Dashboard.DashboardWidget" />
+    [DisplayName( "Lava Sql Dashboard Widget" )]
+    [Category( "Shepherd Church > Misc" )]
+    [Description( "Dashboard Widget from Lava using Sql query" )]
+    [CodeEditorField( "Sql", "The Sql code to run to generate the data for this block.", CodeEditorMode.Sql, defaultValue:"SELECT 'Hello World' AS [Value]", order: 11)]
+    [CodeEditorField( "Lava Template", "The text (or html) to display as a dashboard widget", CodeEditorMode.Lava, CodeEditorTheme.Rock, 200, Order = 12, DefaultValue =
+@"
+{% for row in Rows %}
+    <h1>{{ row.Value }}</h1>
+{% endfor %}
+" )]
+
+    public partial class LavaSqlDashboardWidget : DashboardWidget
+    {
+        /// <summary>
+        /// Gets the rest URL.
+        /// </summary>
+        /// <value>
+        /// The rest URL.
+        /// </value>
+        public string RestUrl
+        {
+            get
+            {
+                string result = ResolveUrl( "~/api/SC_Misc_Blocks/GetHtmlForSqlBlock/" ) + this.BlockCache.Guid.ToString();
+
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Load" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnLoad( System.EventArgs e )
+        {
+            base.OnLoad( e );
+            pnlDashboardTitle.Visible = !string.IsNullOrEmpty( this.Title );
+            pnlDashboardSubtitle.Visible = !string.IsNullOrEmpty( this.Subtitle );
+            lDashboardTitle.Text = this.Title;
+            lDashboardSubtitle.Text = this.Subtitle;
+        }
+    }
+}
---
+++ Rest/Blocks.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web.Http;
+
+using Rock;
+using Rock.Data;
+using Rock.Model;
+using Rock.Rest;
+
+namespace com.shepherdchurch.Misc.Rest
+{
+    public class SC_Misc_BlocksController : ApiControllerBase
+    {
+        #region API Methods
+
+        /// <summary>
+        /// Retrieve the HTML from the SQL query and Lava Template in the block.
+        /// </summary>
+        /// <param name="blockGuid">The GUID that identifies the block to run this query for.</param>
+        /// <returns>An HTML formatted string.</returns>
+        [HttpGet]
+        [System.Web.Http.Route( "api/SC_Misc_Blocks/GetHtmlForSqlBlock/{blockGuid}" )]
+        public string GetHtmlForSqlBlock( Guid blockGuid )
+        {
+            RockContext rockContext = new RockContext();
+            Block block = new BlockService( rockContext ).Get( blockGuid );
+
+            if ( block != null )
+            {
+                block.LoadAttributes();
+
+                string lavaTemplate = block.GetAttributeValue( "LavaTemplate" );
+                string sql = block.GetAttributeValue( "Sql" );
+
+                var results = DbService.GetDataSet( sql.ToString(), CommandType.Text, null, null );
+
+                var dropRows = new List<DataRowDrop>();
+                if ( results.Tables.Count == 1 )
+                {
+                    foreach ( DataRow row in results.Tables[0].Rows )
+                    {
+                        dropRows.Add( new DataRowDrop( row ) );
+                    }
+                }
+
+                Dictionary<string, object> mergeValues = new Dictionary<string, object>();
+                mergeValues.Add( "Rows", dropRows );
+
+                return lavaTemplate.ResolveMergeFields( mergeValues );
+            }
+
+            return string.Format(
+                @"<div class='alert alert-danger'> 
+                    unable to find block_guid: {0}
+                </div>",
+                blockGuid );
+        }
+
+        #endregion
+
+        #region Classes
+
+        /// <summary>
+        ///
+        /// </summary>
+        private class DataRowDrop : DotLiquid.Drop
+        {
+            private readonly DataRow _dataRow;
+
+            public DataRowDrop( DataRow dataRow )
+            {
+                _dataRow = dataRow;
+            }
+
+            public override object BeforeMethod( string method )
+            {
+                if ( _dataRow.Table.Columns.Contains( method ) )
+                {
+                    return _dataRow[method];
+                }
+
+                return null;
+            }
+        }
+
+        #endregion
+    }
+}
