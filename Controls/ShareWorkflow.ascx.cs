393406a7d07a4fd297d4ba71544aeb95f360e3ff
nairdo <nairdo@gmail.com>
2017-08-15 14:31:51 -0700

    + Pull request to change SharerWorkflow export file name from export.json to <WF-TYPE-NAME>_<DATE>.json.

--- Controls/ShareWorkflow.ascx.cs
+++ Controls/ShareWorkflow.ascx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -148,7 +148,7 @@ namespace RockWeb.Plugins.com_shepherdchurch.Misc
             Page.EnableViewState = false;
             Page.Response.Clear();
             Page.Response.ContentType = "application/json";
-            Page.Response.AppendHeader( "Content-Disposition", "attachment; filename=export.json" );
+            Page.Response.AppendHeader( "Content-Disposition", string.Format( "attachment; filename={0}_{1}.json" , workflowType.Name.MakeValidFileName(), RockDateTime.Now.ToString( "yyyyMMddHHmm" ) ) );
             Page.Response.Write( Newtonsoft.Json.JsonConvert.SerializeObject( container ) );
             Page.Response.Flush();
             Page.Response.End();
