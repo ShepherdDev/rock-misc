ea88088726c9c87e4a91521a3feea0e403ba8dac
Daniel Hazelbaker <daniel@blueboxmoon.com>
2017-09-20 17:36:48 -0700

    Added new block for printing deposit slips for cash/check payments on an event.

---
+++ Controls/EventDepositSlips.ascx
@@ -0,0 +1,51 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="EventDepositSlips.ascx.cs" Inherits="RockWeb.Plugins.com_shepherdchurch.Misc.EventDepositSlips" %>
+
+<asp:UpdatePanel ID="upnlContent" runat="server">
+    <ContentTemplate>
+
+        <asp:Panel ID="pnlDetails" runat="server">
+            <div class="panel panel-block">
+                <div class="panel-heading">
+                    <h1 class="panel-title"><i class="fa fa-print"></i> Payments</h1>
+                </div>
+                <div class="panel-body">
+                    <Rock:ModalAlert ID="mdPaymentsGridWarning" runat="server" />
+                    <div class="grid grid-panel">
+                        <Rock:GridFilter ID="fPayments" runat="server" OnDisplayFilterValue="fPayments_DisplayFilterValue">
+                            <Rock:DateRangePicker ID="drpPaymentDateRange" runat="server" Label="Date Range" />
+                            <Rock:RockCheckBoxList ID="cblCurrencyType" runat="server" Label="Currency Type" RepeatDirection="Horizontal" />
+                        </Rock:GridFilter>
+
+                        <Rock:Grid ID="gPayments" runat="server" DisplayType="Full" AllowSorting="true" RowItemText="Payment">
+                            <Columns>
+                                <Rock:SelectField></Rock:SelectField>
+                                <Rock:RockBoundField DataField="AuthorizedPersonAlias.Person.FullNameReversed" HeaderText="Person"
+                                    SortExpression="AuthorizedPersonAlias.Person.LastName,AuthorizedPersonAlias.Person.NickName" />
+                                <Rock:RockBoundField DataField="TransactionDateTime" HeaderText="Date / Time" SortExpression="TransactionDateTime" />
+                                <Rock:CurrencyField DataField="TotalAmount" HeaderText="Amount" SortExpression="TotalAmount" />
+                                <Rock:RockBoundField DataField="FinancialPaymentDetail.CurrencyTypeValue.Value" HeaderText="Currency Type" />
+                                <Rock:RockTemplateFieldUnselected HeaderText="Registrar">
+                                    <ItemTemplate>
+                                        <asp:Literal ID="lRegistrar" runat="server" />
+                                    </ItemTemplate>
+                                </Rock:RockTemplateFieldUnselected>
+                                <Rock:RockTemplateField HeaderText="Registrant(s)">
+                                    <ItemTemplate>
+                                        <asp:Literal ID="lRegistrants" runat="server" />
+                                    </ItemTemplate>
+                                </Rock:RockTemplateField>
+                            </Columns>
+                        </Rock:Grid>
+                    </div>
+                </div>
+            </div>
+
+            <asp:LinkButton ID="btnPrint" runat="server" CssClass="btn btn-primary" OnClick="btnPrint_Click">Print</asp:LinkButton>
+        </asp:Panel>
+
+        <asp:Panel ID="pnlPrint" runat="server" Visible="false">
+            <asp:Literal ID="ltFormattedOutput" runat="server"></asp:Literal>
+        </asp:Panel>
+
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ Controls/EventDepositSlips.ascx.cs
@@ -0,0 +1,346 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.UI.WebControls;
+
+using Rock;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Security;
+using Rock.Web.Cache;
+using Rock.Web.UI;
+using Rock.Web.UI.Controls;
+
+namespace RockWeb.Plugins.com_shepherdchurch.Misc
+{
+    [DisplayName( "Event Deposit Slips" )]
+    [Category( "Shepherd Church > Misc" )]
+    [Description( "Provides a button to assist in printing cash/check deposit slips for event registrations." )]
+
+    [CodeEditorField( "Formatted Template", "The Lava template to use when formatted the display of the deposit slip.", CodeEditorMode.Lava, height: 400, order: 0 )]
+    [ContextAware( typeof( Rock.Model.RegistrationInstance ) )]
+    public partial class EventDepositSlips : RockBlock, ISecondaryBlock
+    {
+        #region Fields
+
+        private List<Registration> PaymentRegistrations;
+        
+        #endregion
+
+        #region Base Method Overrides
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+
+            // this event gets fired after block settings are updated. it's nice to repaint the screen if these settings would alter it
+            this.BlockUpdated += Block_BlockUpdated;
+            this.AddConfigurationUpdateTrigger( upnlContent );
+
+            fPayments.ApplyFilterClick += fPayments_ApplyFilterClick;
+            gPayments.DataKeyNames = new string[] { "Id" };
+            gPayments.Actions.ShowAdd = false;
+            gPayments.ShowActionRow = false;
+            gPayments.RowDataBound += gPayments_RowDataBound;
+            gPayments.GridRebind += gPayments_GridRebind;
+        }
+
+        /// <summary>
+        /// Initialize basic information about the page structure and setup the default content.
+        /// </summary>
+        /// <param name="sender">Object that is generating this event.</param>
+        /// <param name="e">Arguments that describe this event.</param>
+        protected void Page_Load( object sender, EventArgs e )
+        {
+            if ( !IsPostBack )
+            {
+                SetFilter();
+
+                btnPrint_Click( this, null );
+//                BindPaymentsGrid();
+            }
+        }
+
+        /// <summary>
+        /// Allow primary blocks to hide this one. This is common when the primary block goes
+        /// into edit mode.
+        /// </summary>
+        /// <param name="visible">true if this block should be visible, false if it should be hidden.</param>
+        void ISecondaryBlock.SetVisible( bool visible )
+        {
+            pnlDetails.Visible = false;
+        }
+
+        #endregion
+
+        #region Core Methods
+
+        private void SetFilter()
+        {
+            cblCurrencyType.BindToDefinedType( DefinedTypeCache.Read( Rock.SystemGuid.DefinedType.FINANCIAL_CURRENCY_TYPE.AsGuid() ) );
+
+            cblCurrencyType.SetValues( fPayments.GetUserPreference( "Currency Type" ).SplitDelimitedValues().AsIntegerList() );
+
+            drpPaymentDateRange.DelimitedValues = fPayments.GetUserPreference( "Date Range" );
+        }
+
+        /// <summary>
+        /// Binds the payments grid.
+        /// </summary>
+        private void BindPaymentsGrid()
+        {
+            int? instanceId = PageParameter( "RegistrationInstanceId" ).AsIntegerOrNull();
+            if ( instanceId.HasValue )
+            {
+                using ( var rockContext = new RockContext() )
+                {
+                    var currencyTypes = new Dictionary<int, string>();
+                    var creditCardTypes = new Dictionary<int, string>();
+
+                    // If configured for a registration and registration is null, return
+                    int registrationEntityTypeId = EntityTypeCache.Read( typeof( Rock.Model.Registration ) ).Id;
+
+                    // Get all the registrations for this instance
+                    PaymentRegistrations = new RegistrationService( rockContext )
+                        .Queryable( "PersonAlias.Person,Registrants.PersonAlias.Person" ).AsNoTracking()
+                        .Where( r =>
+                            r.RegistrationInstanceId == instanceId.Value &&
+                            !r.IsTemporary )
+                        .ToList();
+
+                    // Get the Registration Ids
+                    var registrationIds = PaymentRegistrations
+                        .Select( r => r.Id )
+                        .ToList();
+
+                    // Get all the transactions relate to these registrations
+                    var qry = new FinancialTransactionService( rockContext )
+                        .Queryable().AsNoTracking()
+                        .Where( t => t.TransactionDetails
+                            .Any( d =>
+                                d.EntityTypeId.HasValue &&
+                                d.EntityTypeId.Value == registrationEntityTypeId &&
+                                d.EntityId.HasValue &&
+                                registrationIds.Contains( d.EntityId.Value ) ) );
+
+                    // Date Range
+                    var drp = new DateRangePicker();
+                    drp.DelimitedValues = fPayments.GetUserPreference( "Date Range" );
+                    if ( drp.LowerValue.HasValue )
+                    {
+                        qry = qry.Where( t => t.TransactionDateTime >= drp.LowerValue.Value );
+                    }
+
+                    if ( drp.UpperValue.HasValue )
+                    {
+                        DateTime upperDate = drp.UpperValue.Value.Date.AddDays( 1 );
+                        qry = qry.Where( t => t.TransactionDateTime < upperDate );
+                    }
+
+                    SortProperty sortProperty = gPayments.SortProperty;
+                    if ( sortProperty != null )
+                    {
+                        if ( sortProperty.Property == "TotalAmount" )
+                        {
+                            if ( sortProperty.Direction == SortDirection.Ascending )
+                            {
+                                qry = qry.OrderBy( t => t.TransactionDetails.Sum( d => ( decimal? ) d.Amount ) ?? 0.00M );
+                            }
+                            else
+                            {
+                                qry = qry.OrderByDescending( t => t.TransactionDetails.Sum( d => ( decimal? ) d.Amount ) ?? 0.0M );
+                            }
+                        }
+                        else
+                        {
+                            qry = qry.Sort( sortProperty );
+                        }
+                    }
+                    else
+                    {
+                        qry = qry.OrderByDescending( t => t.TransactionDateTime ).ThenByDescending( t => t.Id );
+                    }
+
+                    gPayments.SetLinqDataSource( qry.AsNoTracking() );
+                    gPayments.DataBind();
+                }
+            }
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        /// <summary>
+        /// Handles the BlockUpdated event of the control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void Block_BlockUpdated( object sender, EventArgs e )
+        {
+            BindPaymentsGrid();
+        }
+
+        /// <summary>
+        /// Handles the Click event of the control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void btnPrint_Click( object sender, EventArgs e )
+        {
+            var keys = new List<int>();
+            var mergeFields = Rock.Lava.LavaHelper.GetCommonMergeFields( RockPage );
+            var rockContext = new RockContext();
+            var formattedTemplate = GetAttributeValue( "FormattedTemplate" );
+
+            if ( gPayments.SelectedKeys.Count > 0 )
+            {
+                keys = gPayments.SelectedKeys.Cast<int>().ToList();
+            }
+            else
+            {
+                gPayments.AllowPaging = false;
+                BindPaymentsGrid();
+                gPayments.AllowPaging = true;
+
+                foreach ( DataKey k in gPayments.DataKeys )
+                {
+                    keys.Add( ( int ) k.Value );
+                }
+            }
+
+            int instanceId = PageParameter( "RegistrationInstanceId" ).AsInteger();
+            mergeFields.Add( "RegistrationInstance", new RegistrationInstanceService( rockContext ).Get( instanceId ) );
+
+            var transactions = new FinancialTransactionService( rockContext )
+                .Queryable()
+                .Where( t => keys.Contains( t.Id ) )
+                .OrderBy( t => t.TransactionDateTime )
+                .ToList();
+            mergeFields.Add( "Transactions", transactions );
+
+            var currencyTypes = transactions
+                .GroupBy( t => t.FinancialPaymentDetail.CurrencyAndCreditCardType )
+                .Select( g => new {
+                    Title = g.Key,
+                    Amount = g.Sum( t => t.TotalAmount )
+                } )
+                .ToList();
+            mergeFields.Add( "CurrencyTypes", currencyTypes );
+
+            mergeFields.Add( "Total", transactions.Sum( t => t.TotalAmount ) );
+
+            ltFormattedOutput.Text = formattedTemplate.ResolveMergeFields( mergeFields );
+
+            pnlDetails.Visible = false;
+            pnlPrint.Visible = true;
+        }
+
+        /// <summary>
+        /// Handles the ApplyFilterClick event of the fPayments control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void fPayments_ApplyFilterClick( object sender, EventArgs e )
+        {
+            fPayments.SaveUserPreference( "Date Range", drpPaymentDateRange.DelimitedValues );
+            fPayments.SaveUserPreference( "Currency Type", cblCurrencyType.SelectedValues.AsDelimited( "," ) );
+
+            BindPaymentsGrid();
+        }
+
+        /// <summary>
+        /// Fs the payments_ display filter value.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The e.</param>
+        protected void fPayments_DisplayFilterValue( object sender, GridFilter.DisplayFilterValueArgs e )
+        {
+            switch ( e.Key )
+            {
+                case "Date Range":
+                    {
+                        e.Value = DateRangePicker.FormatDelimitedValues( e.Value );
+                        break;
+                    }
+                case "Currency Type":
+                    {
+                        var ids = e.Value.SplitDelimitedValues().AsIntegerList();
+                        e.Value = DefinedTypeCache.Read( Rock.SystemGuid.DefinedType.FINANCIAL_CURRENCY_TYPE.AsGuid() )
+                            .DefinedValues
+                            .Where( dv => ids.Contains( dv.Id ) )
+                            .Select( dv => dv.Value )
+                            .ToList()
+                            .AsDelimited( ", " );
+                        break;
+                    }
+                default:
+                    {
+                        e.Value = string.Empty;
+                        break;
+                    }
+            }
+        }
+
+        /// <summary>
+        /// Handles the GridRebind event of the gPayments control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void gPayments_GridRebind( object sender, EventArgs e )
+        {
+            BindPaymentsGrid();
+        }
+
+        /// <summary>
+        /// Handles the RowDataBound event of the gPayments control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="GridViewRowEventArgs"/> instance containing the event data.</param>
+        private void gPayments_RowDataBound( object sender, GridViewRowEventArgs e )
+        {
+            var transaction = e.Row.DataItem as FinancialTransaction;
+            var lRegistrar = e.Row.FindControl( "lRegistrar" ) as Literal;
+            var lRegistrants = e.Row.FindControl( "lRegistrants" ) as Literal;
+
+            if ( transaction != null && lRegistrar != null && lRegistrants != null )
+            {
+                var registrars = new List<string>();
+                var registrants = new List<string>();
+
+                var registrationIds = transaction.TransactionDetails.Select( d => d.EntityId ).ToList();
+                foreach ( var registration in PaymentRegistrations
+                    .Where( r => registrationIds.Contains( r.Id ) ) )
+                {
+                    if ( registration.PersonAlias != null && registration.PersonAlias.Person != null )
+                    {
+                        var qryParams = new Dictionary<string, string>();
+                        qryParams.Add( "RegistrationId", registration.Id.ToString() );
+                        string url = LinkedPageUrl( "RegistrationPage", qryParams );
+                        registrars.Add( string.Format( "<a href='{0}'>{1}</a>", url, registration.PersonAlias.Person.FullName ) );
+
+                        foreach ( var registrant in registration.Registrants )
+                        {
+                            if ( registrant.PersonAlias != null && registrant.PersonAlias.Person != null )
+                            {
+                                registrants.Add( registrant.PersonAlias.Person.FullName );
+                            }
+                        }
+                    }
+                }
+
+                lRegistrar.Text = registrars.AsDelimited( "<br/>" );
+                lRegistrants.Text = registrants.AsDelimited( "<br/>" );
+            }
+        }
+
+        #endregion
+    }
+}
