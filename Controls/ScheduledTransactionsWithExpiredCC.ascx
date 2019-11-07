2f2e82a39a93626e962d42e0920ecfed8ef14b6a
cabal95 <daniel@blueboxmoon.com>
2019-11-07 15:45:39 -0800

    - added ScheduledTransactionsWithExpiredCC block.

---
+++ Controls/ScheduledTransactionsWithExpiredCC.ascx
@@ -0,0 +1,99 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="ScheduledTransactionsWithExpiredCC.ascx.cs" Inherits="RockWeb.Plugins.com_shepherdchurch.Misc.ScheduledTransactionsWithExpiredCC" %>
+
+<asp:UpdatePanel ID="upnlContent" runat="server">
+    <ContentTemplate>
+        <asp:HiddenField ID="hfCounts" runat="server" />
+
+        <div style="max-width: 320px; margin: auto;">
+            <div style="display: inline-block; float: left;">
+                <Rock:RockTextBox ID="tbDaysBack" runat="server" Label="Days Back" />
+            </div>
+            <div style="display: inline-block; padding-top: 29px; padding-left: 12px; float:left;">
+                <asp:LinkButton ID="lbUpdate" runat="server" CssClass="btn btn-primary" Text="Update" OnClick="lbUpdate_Click" />
+            </div>
+        </div>
+
+        <asp:Panel ID="pnlChart" runat="server" style="height: 200px;">
+             <canvas></canvas>
+        </asp:Panel>
+
+        <asp:Panel ID="pnlGridRenewed" runat="server" style="display: none;">
+            <Rock:Grid ID="gRenewed" runat="server">
+                <Columns>
+                    <Rock:RockBoundField DataField="Name" HeaderText="Person" />
+                    <Rock:DateField DataField="StartDate" HeaderText="Start" />
+                    <Rock:DateField DataField="EndDate" HeaderText="End" />
+                    <Rock:RockBoundField DataField="TotalAmount" HeaderText="Total Amount" />
+                </Columns>
+            </Rock:Grid>
+        </asp:Panel>
+
+        <asp:Panel ID="pnlGridNotRenewed" runat="server" style="display: none;">
+            <Rock:Grid ID="gNotRenewed" runat="server">
+                <Columns>
+                    <Rock:RockBoundField DataField="Name" HeaderText="Person" />
+                    <Rock:DateField DataField="StartDate" HeaderText="Start" />
+                    <Rock:DateField DataField="EndDate" HeaderText="End" />
+                    <Rock:RockBoundField DataField="TotalAmount" HeaderText="Total Amount" />
+                </Columns>
+            </Rock:Grid>
+        </asp:Panel>
+
+        <script>
+            Sys.Application.add_load(function () {
+                var values = JSON.parse($('#<%= hfCounts.ClientID %>').val());
+                var labels = ['Renewed', 'Not Renewed'];
+
+                if (values[0] === 0 && values[1] === 0) {
+                    values[0] = 1;
+                    labels = ['No Transactions'];
+                }
+                var chartData = {
+                    datasets: [{
+                        data: values,
+                        backgroundColor: ['#f7464a', '#46bfbd']
+                    }],
+                    labels: labels
+                };
+
+                var chartOptions = {
+                    series: {
+                        pie: {
+                            show: true
+                        }
+                    },
+                    grid: {
+                        hoverable: true
+                    },
+                    legend: {
+                        show: true,
+                        backgroundColor: 'transparent'
+                    },
+                    maintainAspectRatio: false,
+                    onClick: function (evt, item) {
+                        if (item.length > 0 && item[0]['_model'].label !== 'No Transactions') {
+                            console.log(item);
+                            var idx = item[0]['_index']
+
+                            if (idx === 0) {
+                                $('#<%= pnlGridRenewed.ClientID %>').toggle();
+                                $('#<%= pnlGridNotRenewed.ClientID %>').hide();
+                            }
+                            else {
+                                $('#<%= pnlGridRenewed.ClientID %>').hide();
+                                $('#<%= pnlGridNotRenewed.ClientID %>').toggle();
+                            }
+                        }
+                    }
+                };
+
+                var canvas = $('#<%= pnlChart.ClientID %> canvas').get(0);
+                new Chart(canvas.getContext('2d'), {
+                    type: 'pie',
+                    data: chartData,
+                    options: chartOptions
+                });
+            });
+        </script>
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ Controls/ScheduledTransactionsWithExpiredCC.ascx.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.UI.WebControls;
+
+using Rock;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Security;
+using Rock.Web;
+using Rock.Web.Cache;
+using Rock.Web.UI;
+
+namespace RockWeb.Plugins.com_shepherdchurch.Misc
+{
+    [DisplayName( "Scheduled Transactions With Expired Credit Card" )]
+    [Category( "com_shepherdchurch > Misc" )]
+    [Description( "Shows a chart of how many scheduled transactions with expired credit cards have been renewed." )]
+
+    public partial class ScheduledTransactionsWithExpiredCC : RockBlock
+    {
+        #region Base Method Overrides
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+
+            RockPage.AddScriptLink( "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.bundle.js", false );
+
+            // this event gets fired after block settings are updated. it's nice to repaint the screen if these settings would alter it
+            this.BlockUpdated += Block_BlockUpdated;
+            this.AddConfigurationUpdateTrigger( upnlContent );
+        }
+
+        /// <summary>
+        /// Initialize basic information about the page structure and setup the default content.
+        /// </summary>
+        /// <param name="sender">Object that is generating this event.</param>
+        /// <param name="e">Arguments that describe this event.</param>
+        protected void Page_Load( object sender, EventArgs e )
+        {
+            if ( !IsPostBack )
+            {
+                tbDaysBack.Text = "30";
+                ShowDetails();
+            }
+        }
+
+        #endregion
+
+        #region Core Methods
+
+        private void ShowDetails()
+        {
+            var renewedItems = new List<TransInfo>();
+            var notRenewedItems = new List<TransInfo>();
+
+            using ( var rockContext = new RockContext() )
+            {
+                var limitDate = RockDateTime.Now.AddDays( -( tbDaysBack.Text.AsIntegerOrNull() ?? 30 ) );
+
+                var transactions = new FinancialScheduledTransactionService( rockContext )
+                    .Queryable()
+                    .Include( t => t.FinancialPaymentDetail )
+                    .Include( t => t.AuthorizedPersonAlias )
+                    .Where( t => t.IsActive && t.FinancialPaymentDetail.ExpirationMonthEncrypted != null )
+                    .Where( t => t.EndDate == null || t.EndDate > DateTime.Now )
+                    .AsNoTracking()
+                    .ToList();
+
+                foreach ( var transaction in transactions )
+                {
+                    int? expirationMonthDecrypted = Encryption.DecryptString( transaction.FinancialPaymentDetail.ExpirationMonthEncrypted ).AsIntegerOrNull();
+                    int? expirationYearDecrypted = Encryption.DecryptString( transaction.FinancialPaymentDetail.ExpirationYearEncrypted ).AsIntegerOrNull();
+
+                    if ( !expirationMonthDecrypted.HasValue || !expirationMonthDecrypted.HasValue )
+                    {
+                        continue;
+                    }
+
+                    int expireYear = expirationYearDecrypted.Value;
+                    int expireMonth = expirationMonthDecrypted.Value;
+                    var expireDate = new DateTime( expireYear, expireMonth, 1 );
+
+                    if ( expireDate < limitDate || expireDate >= RockDateTime.Now )
+                    {
+                        continue;
+                    }
+
+                    var newTransactions = transactions
+                        .Where( t => t.Id != transaction.Id )
+                        .Where( t => t.CreatedDateTime > transaction.CreatedDateTime )
+                        .Where( t => t.AuthorizedPersonAlias.PersonId == transaction.AuthorizedPersonAlias.PersonId )
+                        .ToList();
+
+                    if ( newTransactions.Any() )
+                    {
+                        renewedItems.Add( new TransInfo
+                        {
+                            Name = transaction.AuthorizedPersonAlias.Person.FullNameReversed,
+                            StartDate = transaction.StartDate,
+                            EndDate = expireDate,//transaction.EndDate,
+                            TotalAmount = transaction.TotalAmount
+                        } );
+                    }
+                    else
+                    {
+                        notRenewedItems.Add( new TransInfo
+                        {
+                            Name = transaction.AuthorizedPersonAlias.Person.FullNameReversed,
+                            StartDate = transaction.StartDate,
+                            EndDate = expireDate,//transaction.EndDate,
+                            TotalAmount = transaction.TotalAmount
+                        } );
+                    }
+                }
+
+                gRenewed.DataSource = renewedItems;
+                gRenewed.DataBind();
+
+                gNotRenewed.DataSource = notRenewedItems;
+                gNotRenewed.DataBind();
+
+                hfCounts.Value = string.Format( "[{0},{1}]", renewedItems.Count, notRenewedItems.Count );
+            }
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        /// <summary>
+        /// Handles the BlockUpdated event of the control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void Block_BlockUpdated( object sender, EventArgs e )
+        {
+            ShowDetails();
+        }
+
+        /// <summary>
+        /// Handles the Click event of the lbUpdate control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void lbUpdate_Click( object sender, EventArgs e )
+        {
+            ShowDetails();
+        }
+
+        #endregion
+
+        private class TransInfo
+        {
+            public string Name { get; set; }
+
+            public decimal TotalAmount { get; set; }
+
+            public DateTime StartDate { get; set; }
+
+            public DateTime? EndDate { get; set; }
+        }
+    }
+}
